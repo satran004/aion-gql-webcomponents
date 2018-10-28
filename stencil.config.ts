03398f018a08d75dd0029ce34187cf8b2b773346
satran004 <satran004@gmail.com>
2018-10-28 10:21:11 +0800

    pkg name fix

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 
 export const config: Config = {
-  namespace: 'aion-webcomponents',
+  namespace: 'aion-gql-webcomponents',
   outputTargets:[
     {
       type: 'dist'
